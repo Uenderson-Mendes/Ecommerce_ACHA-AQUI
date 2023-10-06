7e37c8769d99cef3af801665fdadc0146754b2ef
Uenderson-Mendes <uemdersonbebe@gmail.com>
2023-10-06 10:26:54 -0300

    close_button

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -3,14 +3,22 @@ import './Cart.css';
 import Cartitem from '../cartitem/Cartitem';
 import appContext from '../../context/Appcontext';
 import formatCurrency from '../../utils/formatCurrency';
+import {GrClose} from 'react-icons/gr';
 
 function Cart(){
-	const {cartitem, isCartvisible} = useContext(appContext);
+	const {cartitem, isCartvisible, setIsCartvisible} = useContext(appContext);
 	const totalprice = 	cartitem.reduce((acc, item) =>item.price + acc,0);
+	
+	
 
 	return(
 		<section className={`cart ${isCartvisible ? '':'cart--active'}`} >
 			<div className="cart-itens">
+				<div className="fechar">
+					<button type="button" className="Cart__button" onClick={() =>setIsCartvisible(!isCartvisible)}>
+						<GrClose/>
+					</button>
+				</div>
 				{cartitem.map((cartItem)=> <Cartitem key={cartItem} data={cartItem}/>)}
 				
 			</div>
